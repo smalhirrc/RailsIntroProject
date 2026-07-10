fb43bf37c3c4a3686138f367ee5c1b5613e1e4b7
Sukhpreet's MacBook <sukhpreetsmacbook@Sukhpreetss-MacBook-Air.local>
2026-07-09 20:56:38 -0500

    seed script loaded with csv data, ready to populate tables

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,17 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require 'csv'
+
+Asset.destroy_all
+District.destroy_all
+Park.destroy_all
+
+park_csv_file = Rails.root.join('db/ParksAndOpenSpaceData.csv')
+park_asset_csv_file = Rails.root.join('db/ParkAssetInventoryData.csv')
+
+park_data = File.read(park_csv_file)
+park_asset_data = File.read(park_asset_csv_file)
+
+parks = CSV.parse(park_data, headers: true)
+assets =  CSV.parse(park_asset_data, headers: true)
