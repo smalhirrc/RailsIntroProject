23d58f6b433da03a333bef29945da6a0976d8d91
Sukhpreet's MacBook <sukhpreetsmacbook@Sukhpreetss-MacBook-Air.local>
2026-07-10 03:45:03 -0500

    member pages and multi model data on member pages

--- app/controllers/parks_controller.rb
+++ app/controllers/parks_controller.rb
@@ -4,4 +4,9 @@ class ParksController < ApplicationController
 
   def about
   end
+
+  def show
+    @park = Park.find_by(park_id: params[:id])
+    @asset = Asset.where(park_id: params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
 
   get "/index", to: "parks#index"
   get "/about", to: "parks#about"
+  get "/show/:id", to: "parks#show"
 
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
